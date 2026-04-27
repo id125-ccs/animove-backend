40d52642408d350f00b5900a6fb8136c641c5ba2
Andrei Sumilang <sumilangandreijohn@gmail.com>
2026-04-27 19:36:17 +0800

    fix(domain/category): enforce Category invariants during initialization

--- domain/src/main/kotlin/ph/edu/dlsu/animove/domain/category/Category.kt
+++ domain/src/main/kotlin/ph/edu/dlsu/animove/domain/category/Category.kt
@@ -35,4 +35,9 @@ class Category(
             require(value > 0) { "Reservation capacity must be greater than 0" }
             field = value
         }
+
+    init {
+        this.name = name
+        this.reservationCapacity = reservationCapacity
+    }
 }
