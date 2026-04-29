c2f69a3faa96fda319e9dcf709a75e92b1da9258
Andrei Sumilang <sumilangandreijohn@gmail.com>
2026-04-29 19:39:37 +0800

    refactor(domain/coordinate): extract latitude and longitude bounds into constants

--- domain/src/main/kotlin/ph/edu/dlsu/animove/domain/shared/Coordinate.kt
+++ domain/src/main/kotlin/ph/edu/dlsu/animove/domain/shared/Coordinate.kt
@@ -14,8 +14,19 @@ data class Coordinate(
     val latitude: Double,
     val longitude: Double
 ) {
+    companion object {
+        const val MINIMUM_LATITUDE = -90.0
+        const val MAXIMUM_LATITUDE = 90.0
+        const val MINIMUM_LONGITUDE = -180.0
+        const val MAXIMUM_LONGITUDE = 180.0
+    }
+
     init {
-        require(latitude in -90.0..90.0) { "Latitude must be between -90 and 90" }
-        require(longitude in -180.0..180.0) { "Longitude must be between -180 and 180" }
+        require(latitude in MINIMUM_LATITUDE..MAXIMUM_LATITUDE) {
+            "Latitude must be between $MINIMUM_LATITUDE and $MAXIMUM_LATITUDE"
+        }
+        require(longitude in MINIMUM_LONGITUDE..MAXIMUM_LONGITUDE) {
+            "Longitude must be between $MINIMUM_LONGITUDE and $MAXIMUM_LONGITUDE"
+        }
     }
 }
