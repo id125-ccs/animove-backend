e0d8db7174eb67a3cf10a31a04bfafb6bd63c8a0
Andrei Sumilang <sumilangandreijohn@gmail.com>
2026-06-22 19:14:07 +0800

    feat(application/category): add handler for rename category command

---
+++ application/src/main/kotlin/application/category/handler/RenameCategoryHandler.kt
@@ -0,0 +1,34 @@
+package application.category.handler
+
+import application.category.command.RenameCategoryCommand
+import ph.edu.dlsu.animove.domain.category.CategoryRepository
+import ph.edu.dlsu.animove.domain.error.DomainError
+import ph.edu.dlsu.animove.domain.shared.Name
+
+sealed interface RenameCategoryResult {
+    object Success : RenameCategoryResult
+
+    object NotFound : RenameCategoryResult
+
+    data class ValidationFailed(val error: DomainError) : RenameCategoryResult
+
+    data class InternalError(val cause: Throwable) : RenameCategoryResult
+}
+
+class RenameCategoryHandler(
+    private val categoryRepository: CategoryRepository
+) {
+    fun handle(command: RenameCategoryCommand): RenameCategoryResult = try {
+        val category = categoryRepository.findById(command.categoryId) ?: return RenameCategoryResult.NotFound
+
+        category.name = Name.create(command.newName)
+
+        categoryRepository.save(category)
+
+        RenameCategoryResult.Success
+    } catch (e: DomainError) {
+        RenameCategoryResult.ValidationFailed(e)
+    } catch (e: Throwable) {
+        RenameCategoryResult.InternalError(e)
+    }
+}
